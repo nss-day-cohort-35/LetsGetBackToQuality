c17c33c6281bd4cb72c351d9a69d821c546cbef0
unknown <mariobud@gmail.com>
2019-09-19 14:55:26 -0500

    changes

--- src/scripts/friends/eventListeners.js
+++ src/scripts/friends/eventListeners.js
@@ -1,5 +1,5 @@
 const chatAndFriends = {
-    returnFriendArray: function(mainUserNum) {
+    returnFriendArray: function (mainUserNum) {
         return fetch("http://localhost:8088/friends/?friendInitiate=1&_expand=user")
             .then(data => data.json())
             .then(parsedData => {
@@ -12,6 +12,41 @@ const chatAndFriends = {
                     objectArray.push(friendObject);
                 });
                 return objectArray
+            })
+    },
+
+    returnMessagesArray: function (fetchedArray, mainUserNum) {
+        let fetchString = `http://localhost:8088/messages?_expand=user&_sort=date&_order=asc&userId=${mainUserNum}&`
+        fetchedArray.forEach(element => {
+            const concatingString = "userId=" + element.userNum + "&"
+            fetchString = fetchString.concat(concatingString)
+        });
+
+        return fetch(fetchString)
+            .then(data => data.json())
+            .then(parsedData => {
+                document.querySelector("#chat-room").innerHTML += ""
+                console.table(parsedData);
+                parsedData.forEach(element => {
+                    if (mainUserNum === element.userId) {
+                        document.querySelector("#chat-room").innerHTML +=
+                            `
+                        <div id = "message-${element.userId}" class = "message">
+                        <p id = "userId-${element.userId}">${element.user.userName}</p>
+                        <p id = "message-${element.userId}">${element.message}</p>
+                        <button id = "edit-${element.userId}">Edit</button>
+                        <p id "date-${element.userId}">${element.date}</p>
+                    `
+                    }
+                    else {
+                        `
+                        <div id = "message-${element.userId}" class = "message">
+                        <p id = "userId-${element.userId}">${element.user.userName}</p>
+                        <p id = "message-${element.userId}">${element.message}</p>
+                        <p id "date-${element.userId}">${element.date}</p>
+                        `
+                    }
+                })
             })
     }
 }
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,4 @@
-import test from "./friends/eventListeners"
+import chatAndFriends from "./friends/eventListeners"
 /*
     Import all the tools into main.js that are needed to display
     the initial UI to the user. Either the login form should appear
@@ -7,36 +7,11 @@ import test from "./friends/eventListeners"
 
 //**********************
 
-const returnMessagesArray = (fetchedArray) => {
-    let fetchString = "http://localhost:8088/messages?_expand=user&_sort=date&_order=asc&userId=1&"
-    fetchedArray.forEach(element => {
-        const concatingString = "userId=" + element.userNum + "&"
-        fetchString = fetchString.concat(concatingString)
-    });
-
-    return fetch(fetchString)
-        .then(data => data.json())
-        .then(parsedData => {
-            parsedData.forEach(element => {
-                console.table(element)
-                document.querySelector("#chat-room").innerHTML +=
-                `
-                <div id = "message-${element.userId}" class = "message">
-                    <p id = "userId-${element.userId}">${element.user.userName}</p>
-                    <p id = "message-${element.userId}">${element.message}</p>
-                    <button id = "edit-${element.userId}">Edit</button>
-                    <p id "date-${element.userId}">${element.date}</p>
-                </div>
-                `
-            });
-        })
-}
-
 var friendArray = []
 
-returnFriendArray().then(data => {
+chatAndFriends.returnFriendArray(1).then(data => {
     friendArray = data
-    returnMessagesArray(friendArray);
+    chatAndFriends.returnMessagesArray(friendArray,1);
 })
 
 
