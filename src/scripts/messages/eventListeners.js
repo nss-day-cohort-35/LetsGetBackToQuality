469fa8f77cb73d2a1012c34abeaa25c4b1308fc3
unknown <mariobud@gmail.com>
2019-09-23 10:42:53 -0500

    Commiting changes

--- src/scripts/messages/eventListeners.js
+++ src/scripts/messages/eventListeners.js
@@ -1,16 +1,31 @@
 const messageEventListener = {
     addChat: function(userIdNum) {
 
-        const date = Date();
+        const date = Date().split(" ");
+        const month = [
+            01,
+            02,
+            03,
+            04,
+            05,
+            06,
+            07,
+            08,
+            09,
+            10,
+            11,
+            12
+        ]
 
-
-    /*
         const addedChat = {
             userId: parseInt(userIdNum),
-            date: "2019-09-01",
-            "message": "Content of first message"
+            date: `${date[3]}-${month[Date.getMonth()]}-${date[2]}`,
+            message: document.querySelector("#message-box").value
         }
 
+        console.log(addedChat);
+
+        /*
         fetch("http://localhost:8088/friends", {
             method: "POST",
             headers: {
@@ -18,8 +33,8 @@ const messageEventListener = {
             },
             body: JSON.stringify(addedFriend)
         })
-
         */
+
     }
 }
 
